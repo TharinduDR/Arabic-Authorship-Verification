429036a9d6615237fdea8ae837a6def62050e0cb
TharinduDR <rhtdranasinghe@gmail.com>
2020-04-27 12:44:32 +0100

    001: Initial Commit

--- algo/run_experiment.py
+++ algo/run_experiment.py
@@ -8,14 +8,12 @@ import os
 from tqdm import tqdm
 
 from preprocess.cleaning import clean_arabic
+from utility.split import batch
 
 
-def run_use_experiment(list_1, list_2, result_file, optimize=False, cleaning=True, random_seed=777):
+def run_use_experiment(list_1, list_2, result_file, batch_size=8, optimize=False, cleaning=True, random_seed=777):
     shuffle(list_1)
     shuffle(list_2)
-
-    list_1 = list_1[:8]
-    list_2 = list_2[:8]
 
     if os.path.isfile(result_file):
         os.remove(result_file)
@@ -26,18 +24,31 @@ def run_use_experiment(list_1, list_2, result_file, optimize=False, cleaning=Tru
     else:
         embed = hub.load("https://tfhub.dev/google/universal-sentence-encoder-multilingual-large/3")
 
+    list_1_embeddings = []
+    list_2_embeddings = []
+
     if cleaning:
         cleaned_list_1 = [clean_arabic(item) for item in list_1]
-        list_1_embeddings = embed(cleaned_list_1)
-        print("Length of the list 1 embeddings {}".format(str(len(list_1_embeddings))))
         cleaned_list_2 = [clean_arabic(item) for item in list_2]
-        list_2_embeddings = embed(cleaned_list_2)
+
+        for x in batch(cleaned_list_1, batch_size):
+            list_1_embeddings.append(embed(x))
+
+        print("Length of the list 1 embeddings {}".format(str(len(list_1_embeddings))))
+
+        for x in batch(cleaned_list_2, batch_size):
+            list_2_embeddings.append(embed(x))
+
         print("Length of the list 2 embeddings {}".format(str(len(list_2_embeddings))))
 
     else:
-        list_1_embeddings = embed(list_1)
+        for x in batch(list_1, batch_size):
+            list_1_embeddings.append(embed(x))
         print("Length of the list 1 embeddings {}".format(str(len(list_1_embeddings))))
-        list_2_embeddings = embed(list_2)
+
+        for x in batch(list_2, batch_size):
+            list_2_embeddings.append(embed(x))
+
         print("Length of the list 2 embeddings {}".format(str(len(list_2_embeddings))))
 
     closest_n = 5
